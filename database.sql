a76ee4b3d0c3b5c4e4552d8254491e8d1066f986
garik444 <gghevondyan21@gmail.com>
2026-06-16 10:38:25 +0400

    Create users table and insert initial admin user
    Replaced student and project tables with users table and added initial admin user.

--- database.sql
+++ database.sql
@@ -1,17 +1,7 @@
-CREATE TABLE student (
-    id INT PRIMARY KEY,
-    name VARCHAR(100)
+CREATE TABLE users (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(50),
+    password VARCHAR(50)
 );
-
-CREATE TABLE project (
-    id INT PRIMARY KEY,
-    title VARCHAR(100)
-);
-
-CREATE TABLE student_project (
-    student_id INT,
-    project_id INT,
-    PRIMARY KEY(student_id, project_id),
-    FOREIGN KEY(student_id) REFERENCES student(id),
-    FOREIGN KEY(project_id) REFERENCES project(id)
-);
+INSERT INTO users(username,password)
+VALUES ('admin','1234');
